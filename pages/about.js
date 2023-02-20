489bc24b17fe1b3d569baa6fa3d1e137d4d12f1d
laszej <stanislawlaskowski87@gmail.com>
2023-02-20 01:36:19 +0100

    upa

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,10 @@ const About = () => {
   return <div> <h5>Witam na mojej stronie internetowej!
 
 
-  Witryna składa się z dwóch części. Pierwszą jest portfolio, w którym umieszczam stworzone przez siebie projekty. Na dzień dzisiejszy, wszystkie są napisane w "czystej triadzie" tj. HTML, CSS oraz JavaScript. Niektóre z nich są są dosyć proste, inne wymagały sporo cierpliwości i czasu jak np. "Amazorron", który będąc dość złożoną aplikacją, napisałem bardzo imperatywnym kodem JS, ale też mocno testując przy okazji swoje umiejętności manipulowania Document Object Model poprzez JavaScript. W przyszłości do tworzenia tego typu projektów będę korzystał z React.js, który jest moim aktualnym kierunkiem rozwoju, i który znacznie przyśpiesza i ułatwia budowanie złożonych aplikacji.
+  Witryna składa się z dwóch części. Pierwszą jest portfolio, w którym umieszczam stworzone przez siebie projekty. Na dzień dzisiejszy, wszystkie są napisane w czystej triadzie tj. HTML, CSS oraz JavaScript. Niektóre z nich są są dosyć proste, inne wymagały sporo cierpliwości i czasu jak np. Amazorron, który będąc dość złożoną aplikacją, napisałem bardzo imperatywnym kodem JS, ale też mocno testując przy okazji swoje umiejętności manipulowania Document Object Model poprzez JavaScript. W przyszłości do tworzenia tego typu projektów będę korzystał z React.js, który jest moim aktualnym kierunkiem rozwoju, i który znacznie przyśpiesza i ułatwia budowanie złożonych aplikacji.
 
 
-  Druga część to prezentacja mojej osoby, ale przede wszystkim odpowiedź na pytania, dlaczego front-end i czy to aby na pewno wszystko czym chcę się zajmować? W jakim kierunku zmierzam i dlaczego? Na te pytania znajdziecie Państwo odpowiedzi w podstronie o mnie</h5></div>;
+  Druga część to prezentacja mojej osoby, ale przede wszystkim odpowiedź na pytania, dlaczego front-end i czy to aby na pewno wszystko czym chcę się zajmować? W jakim kierunku zmierzam i dlaczego? Na te pytania znajdziecie Państwo odpowiedzi w podstronie o mnie</h5></div>
 };
 
 export default About;
