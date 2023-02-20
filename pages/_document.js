51234bbe4b7e5f3af28b3700165b84295f0f6fd1
laszej <stanislawlaskowski87@gmail.com>
2023-02-20 02:51:16 +0100

    pipa jasia

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet" />
+        <style jsx global>{`@import url("https://fonts.googleapis.com/css2?family=Ubuntu&display=swap");
+  body {
+    font-family: 'Ubuntu', sans-serif;
+  }
+`}</style>
         </Head>
         <body>
           <Main />
