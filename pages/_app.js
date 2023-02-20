93e058c1c45df459257151b837a261a254f8df29
laszej <stanislawlaskowski87@gmail.com>
2023-02-20 23:51:05 +0100

    kiurwa

--- pages/_app.js
+++ pages/_app.js
@@ -3,28 +3,39 @@ import "../styles/globals.css";
 import { useEffect } from "react";
 import Head from "next/head";
 import Layout from "../components/Layout";
+import { Ubuntu } from '@next/font/google'
+import 'typeface-ubuntu'
+
+const ubuntu = Ubuntu({
+  weight: "400",
+  subsets: ['latin-ext', 'latin'],
+  weight: ['300', '400', '600', '700'],
+  display: 'swap'
+});
 
 function MyApp({ Component, pageProps }) {
 
   useEffect(()=>{
     import("bootstrap/dist/js/bootstrap");
-    import ('https://fonts.googleapis.com/css2?family=Ubuntu:wght@400&display=swap');
   },[])
 
   return (
-  
-  <>
+    <>
+    <style jsx global>{`
+        html  {
+          font-family: ${ubuntu.style.fontFamily};
+        }
+      `}</style>
       <Head>
-    
+    <link rel="stylesheet" href={ubuntu} />
+     <html lang="pl" />
     </Head>
 
    <Layout>
      
       <Component {...pageProps} />
     </Layout>
-
     </>
-   
   );
 }
 
