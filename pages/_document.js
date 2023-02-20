c28ec411effc0800e5d6b4132aa6818caba6142b
laszej <stanislawlaskowski87@gmail.com>
2023-02-20 15:39:21 +0100

    something1

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,12 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet" />
-        <style jsx global>{`@import url("https://fonts.googleapis.com/css2?family=Ubuntu&display=swap");
-  body {
-    font-family: 'Ubuntu', sans-serif;
-  }
-`}</style>
+        <style>
+         {`@import url("https://fonts.googleapis.com/css2?family=Ubuntu&display=swap");
+          body {
+        font-family: 'Ubuntu', sans-serif;
+        }`}
+  </style>
         </Head>
         <body>
           <Main />
