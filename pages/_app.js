7491f24acf88d1848938604a9e3c0d7ccc26796d
laszej <stanislawlaskowski87@gmail.com>
2023-02-21 18:11:59 +0100

    something

--- pages/_app.js
+++ pages/_app.js
@@ -3,25 +3,23 @@ import "../styles/globals.css";
 import { useEffect } from "react";
 import Head from "next/head";
 import Layout from "../components/Layout";
-import { Ubuntu } from '@next/font/google'
-//import 'typeface-ubuntu'
-
-//const ubuntu = Ubuntu({ weight: "400", subsets: ['latin', 'latin-ext'], display: 'swap' });
-
 function MyApp({ Component, pageProps }) {
   useEffect(()=>{
     import("bootstrap/dist/js/bootstrap");
-  },[])
-
+},[])
+ 
   return (
-    <>
-     
+   
+
+    <Layout>
+      <Head>
+      
+
+      </Head>
+      <Component {...pageProps} />
+
+    </Layout>
     
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
   );
 }
-
 export default MyApp;
