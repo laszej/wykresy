86eb2a08b5e17933a0a7e282c08b8f61ce6472da
laszej <stanislawlaskowski87@gmail.com>
2023-03-02 22:41:34 +0100

    l

--- pages/reactnext.js
+++ pages/reactnext.js
@@ -81,7 +81,7 @@ function ReactNext() {
         </div>
 
         <div className={`${styles.projectsWrapper} ${isVisible[1] ? styles["is-visible"] : ""}`} ref={refs[1]}>
-        <div className={styles.projects}><p><strong>Odgadnij nazwę Państwa</strong><br/><br/>Znasz wszystkie? :) Zadbałem o to, by pojawiły się w grze nazwy państw, o których wielu nie słyszało, ale spokojnie - wystarczy cierpliwość w podążaniu za prawidłową nazwą!</p>Przetestuj<Link href='https://laszej.github.io/gra2/' target='blank'> <strong>tutaj</strong><br/><br/></Link><img src='/projects/odgadnij.png' className={styles.pImages} /></div></div>
+        <div className={styles.projects}><p><strong>Odgadnij nazwę Państwaaaa</strong><br/><br/>Znasz wszystkie? :) Zadbałem o to, by pojawiły się w grze nazwy państw, o których wielu nie słyszało, ale spokojnie - wystarczy cierpliwość w podążaniu za prawidłową nazwą!</p>Przetestuj<Link href='https://laszej.github.io/gra2/' target='blank'> <strong>tutaj</strong><br/><br/></Link><img src='/projects/odgadnij.png' className={styles.pImages} /></div></div>
       
       </div>
 
