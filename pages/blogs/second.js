f262166b4fc29d95223ea0df733a6459876720a6
laszej <stanislawlaskowski87@gmail.com>
2023-03-30 19:04:57 +0200

    l

---
+++ pages/blogs/second.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import SecondGallery from '../../components/SecondGallery';
+import styles from './blogs.module.css'
+import Comments from '../../components/Comments';
+import Head from 'next/head';
+
+const blog0 = [
+  { src: '/blog1/1.jpg', alt: 'Image 1' },
+  { src: '/blog1/2.jpg', alt: 'Image 2' },
+  
+];
+
+const Second = () => {
+  return (
+    <>
+    <Head>
+      <title>Bartłomiej Wróblewski pełnomocnikiem w Poznaniu</title>
+      <meta name="description" content="Spotkanie inaugurujące przejęcie pełnomocnictwa w okręgu poznańskim przez Pana posła Bartłomieja Wróblewskiego." />
+      <meta property="og:title" content="Bartłomiej Wróblewski pełnomocnikiem w Poznaniu" />
+      <meta property="og:description" content="Spotkanie inaugurujące przejęcie pełnomocnictwa w okręgu poznańskim przez Pana posła Bartłomieja Wróblewskiego." />
+      <meta property="og:image" content="/blog1/2.jpg" />
+    </Head>
+     <div className={styles.projects} id="post1">
+      <p>24.11.2022 r.</p>
+      <h5 style={{textAlign: "center"}}><strong>Bartłomiej Wróblewski pełnomocnikiem w Poznaniu</strong></h5>
+        
+        <br />
+        <br />
+        
+      <h5>Spotkanie inaugurujące przejęcie pełnomocnictwa w okręgu poznańskim przez Pana posła Bartłomieja Wróblewskiego. <br/><br/>
+      Poznański parlamentarzysta jest bodaj jedyną osobą w Polsce, która będzie zarządzała dwoma okręgami (już wcześniej okręgiem - powiat poznański)
+      <br/><br/>
+      Nowy pełnomocnik jest od dawna znany ze swej energii, pracowitości i doskonałej organizacji. Bardzo dużo wymaga, przede wszystkim od siebie, ale energię zagrzewania do walki i mobilizowania czuł każdy na sali. W trakcie spotkania kilkoro sympatyków złożyło deklaracje członkowskie i niebawem staną się częścią naszych struktur, które Bartłomiej Wróblewski z pewnością szybko wzmocni i zreorganizuje. 
+      <br/><br/>
+      Osobiście jestem bardzo zadowolony z tej energii, którą Bartek wnosi, choć dokładnie tego się spodziewałem!
+
+
+      </h5> 
+      <div style={{display: "flex", justifyContent:"center",marginTop:"2rem"}}>
+      <SecondGallery images={blog0} />
+     
+      </div>
+      <Comments collection="PoliticalBlog"/>
+     </div>
+     </>
+  
+  )
+}
+
+export default Second
