166688527154c4b65096b72c42d8e60df199a976
laszej <stanislawlaskowski87@gmail.com>
2023-03-31 18:55:24 +0200

    l

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
 const index = () => {
   return (
-    <div><h1>Wybierz konkretny adres, by wyświetlić bloga</h1></div>
+    <div><h1>Wybierz konkretny adres, by wyświetlić bloga</h1>
+    <p>Niektóre z dostępnych blogów:</p>
+    <Link href="/blogs/second">Minister Czarnek w Poznaniu</Link>
+    <br/><br/>
+    <Link href="/blogs/first">Nowy Pełnomocnik</Link>
+    </div>
   )
 }
 
