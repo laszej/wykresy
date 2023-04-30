380c2a837ddfa7f405734fefb7b05c9da45bc7ef
laszej <stanislawlaskowski87@gmail.com>
2023-04-30 17:45:02 +0200

    chuj ci w dupala

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,10 +1,24 @@
-import { connectToDatabase } from "../../utils/mongodb";
+import express from 'express';
+import { connectToDatabase } from '../../utils/mongodb';
+import cors from 'cors';
 
-export default async function handler(req, res) {
+const app = express();
+
+// Set up CORS options
+const corsOptions = {
+  origin: '*', // allow requests from all origins
+  methods: ['GET'], // only allow GET requests
+};
+
+app.use(cors(corsOptions));
+
+app.get('/api/blogs', async (req, res) => {
   const { collection } = req.query;
   const { db } = await connectToDatabase();
 
   const blogs = await db.collection(collection).find({}).toArray();
 
   res.status(200).json(blogs);
-}
+});
+
+export default app;
