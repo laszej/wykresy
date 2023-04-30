cd1b4f9d4ec83f6648ec0107719cf3c8ef1035f4
laszej <stanislawlaskowski87@gmail.com>
2023-04-30 17:12:34 +0200

    :

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,6 +1,16 @@
 import { connectToDatabase } from "../../utils/mongodb";
+import cors from "cors";
+
+// Set up CORS options
+const corsOptions = {
+  origin: "*", // allow requests from all origins
+  methods: ["GET"], // only allow GET requests
+};
 
 export default async function handler(req, res) {
+  // Apply CORS middleware
+  await cors(corsOptions)(req, res);
+
   const { collection } = req.query;
   const { db } = await connectToDatabase();
 
